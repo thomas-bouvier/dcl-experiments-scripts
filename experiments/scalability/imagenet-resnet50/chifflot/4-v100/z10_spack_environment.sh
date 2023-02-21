6d9a16679344d9b29c42973bb5bb5f03863affee
Thomas Bouvier <contact@thomas-bouvier.io>
2023-02-21 10:07:18 +0100

    feat: add gemini deployment without docker

--- experiments/scalability/imagenet-resnet50/chifflot/4-v100/z10_spack_environment.sh
+++ experiments/scalability/imagenet-resnet50/chifflot/4-v100/z10_spack_environment.sh
@@ -1,7 +1,7 @@
-export SPACK_ENV=/root/spack-envs/chifflot;
+export SPACK_ENV=/root/spack-envs/chifflot-v100;
 alias despacktivate='spack env deactivate';
-export ACLOCAL_PATH=/mnt/view/chifflot/share/aclocal;
-export CMAKE_PREFIX_PATH=/mnt/view/chifflot;
-export MANPATH=/mnt/view/chifflot/share/man:/mnt/view/chifflot/man:/usr/share/lmod/lmod/share/man:.:;
-export PATH=/mnt/view/chifflot/bin:/root/spack/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/cuda/bin:/opt/rocm/bin;
-export PKG_CONFIG_PATH=/mnt/view/chifflot/share/pkgconfig:/mnt/view/chifflot/lib64/pkgconfig:/mnt/view/chifflot/lib/pkgconfig;
+export ACLOCAL_PATH=/mnt/view/chifflot-v100/share/aclocal;
+export CMAKE_PREFIX_PATH=/mnt/view/chifflot-v100;
+export MANPATH=/mnt/view/chifflot-v100/share/man:/mnt/view/chifflot-v100/man:/usr/share/lmod/lmod/share/man:.:;
+export PATH=/mnt/view/chifflot-v100/bin:/root/spack/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/cuda/bin:/opt/rocm/bin;
+export PKG_CONFIG_PATH=/mnt/view/chifflot-v100/share/pkgconfig:/mnt/view/chifflot-v100/lib64/pkgconfig:/mnt/view/chifflot-v100/lib/pkgconfig;
