29e47a5ed89a6c3245d58e345e7c46f254381034
Thomas Bouvier <contact@thomas-bouvier.io>
2023-07-30 14:43:22 +0200

    feat: do not mount group storage

--- experiments/scalability/imagenet-resnet50/chifflot/2-v100/z10_spack_environment.sh
+++ experiments/scalability/imagenet-resnet50/chifflot/2-v100/z10_spack_environment.sh
@@ -1,7 +1,7 @@
 export SPACK_ENV=/root/spack-envs/chifflot/v100;
 alias despacktivate='spack env deactivate';
-export ACLOCAL_PATH=/mnt/view/chifflot-v100/share/aclocal;
-export CMAKE_PREFIX_PATH=/mnt/view/chifflot-v100;
-export MANPATH=/mnt/view/chifflot-v100/share/man:/mnt/view/chifflot-v100/man:/usr/share/lmod/lmod/share/man:.:;
-export PATH=/mnt/view/chifflot-v100/bin:/root/spack/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/cuda/bin:/opt/rocm/bin;
-export PKG_CONFIG_PATH=/mnt/view/chifflot-v100/share/pkgconfig:/mnt/view/chifflot-v100/lib64/pkgconfig:/mnt/view/chifflot-v100/lib/pkgconfig;
+export ACLOCAL_PATH=/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-v100/share/aclocal;
+export CMAKE_PREFIX_PATH=/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-v100;
+export MANPATH=/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-v100/share/man:/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-v100/man:/usr/share/lmod/lmod/share/man:.:;
+export PATH=/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-v100/bin:/root/spack/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/cuda/bin:/opt/rocm/bin;
+export PKG_CONFIG_PATH=/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-v100/share/pkgconfig:/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-v100/lib64/pkgconfig:/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-v100/lib/pkgconfig;
--- experiments/scalability/imagenet-resnet50/gemini/8/z10_spack_environment.sh
+++ experiments/scalability/imagenet-resnet50/gemini/8/z10_spack_environment.sh
@@ -1,7 +1,7 @@
-export SPACK_ENV=/root/spack-envs/gemini;
+export SPACK_ENV=/home/tbouvier/spack-lyon/spack-envs/gemini;
 alias despacktivate='spack env deactivate';
-export ACLOCAL_PATH=/mnt/view/gemini/share/aclocal;
-export CMAKE_PREFIX_PATH=/mnt/view/gemini;
-export MANPATH=/mnt/view/gemini/share/man:/mnt/view/gemini/man:/usr/share/lmod/lmod/share/man:.:;
-export PATH=/mnt/view/gemini/bin:/root/spack/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/cuda/bin:/opt/rocm/bin;
-export PKG_CONFIG_PATH=/mnt/view/gemini/share/pkgconfig:/mnt/view/gemini/lib64/pkgconfig:/mnt/view/gemini/lib/pkgconfig;
+export ACLOCAL_PATH=/srv/storage/kerdatadatasets@storage1.lyon.grid5000.fr/view/gemini/share/aclocal;
+export CMAKE_PREFIX_PATH=/srv/storage/kerdatadatasets@storage1.lyon.grid5000.fr/view/gemini;
+export MANPATH=/srv/storage/kerdatadatasets@storage1.lyon.grid5000.fr/view/gemini/share/man:/srv/storage/kerdatadatasets@storage1.lyon.grid5000.fr/view/gemini/man:/usr/share/lmod/lmod/share/man:.:;
+export PATH=/srv/storage/kerdatadatasets@storage1.lyon.grid5000.fr/view/gemini/bin:/home/tbouvier/spack-lyon/spack/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/cuda/bin:/opt/rocm/bin;
+export PKG_CONFIG_PATH=/srv/storage/kerdatadatasets@storage1.lyon.grid5000.fr/view/gemini/share/pkgconfig:/srv/storage/kerdatadatasets@storage1.lyon.grid5000.fr/view/gemini/lib64/pkgconfig:/srv/storage/kerdatadatasets@storage1.lyon.grid5000.fr/view/gemini/lib/pkgconfig;
