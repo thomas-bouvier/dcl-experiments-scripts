6697288b2243317a42b1f438ac537ab26f953bcb
Thomas Bouvier <contact@thomas-bouvier.io>
2023-08-01 16:20:49 +0200

    feat: add first hparams search

---
+++ e2clab/hparams/chifflot/2/z10_spack_environment.sh
@@ -0,0 +1,7 @@
+export SPACK_ENV=/home/tbouvier/spack-lille/spack-envs/chifflot/p100;
+alias despacktivate='spack env deactivate';
+export ACLOCAL_PATH=/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-p100/share/aclocal;
+export CMAKE_PREFIX_PATH=/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-p100;
+export MANPATH=/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-p100/share/man:/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-p100/man:/usr/share/lmod/lmod/share/man:.:;
+export PATH=/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-p100/bin:/root/spack/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/cuda/bin:/opt/rocm/bin;
+export PKG_CONFIG_PATH=/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-p100/share/pkgconfig:/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-p100/lib64/pkgconfig:/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-p100/lib/pkgconfig;
