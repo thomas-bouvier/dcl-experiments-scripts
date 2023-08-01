bc5a899cc4164e78da4d999db5ef1dba66eabd5e
Thomas Bouvier <contact@thomas-bouvier.io>
2023-08-01 19:07:01 +0200

    feat: don't fail when neomem build doesn't pass

---
+++ e2clab/hparams/chifflot/2-v100/z10_spack_environment.sh
@@ -0,0 +1,7 @@
+export SPACK_ENV=/home/tbouvier/spack-lille/spack-envs/chifflot/v100;
+alias despacktivate='spack env deactivate';
+export ACLOCAL_PATH=/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-v100/share/aclocal;
+export CMAKE_PREFIX_PATH=/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-v100;
+export MANPATH=/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-v100/share/man:/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-v100/man:/usr/share/lmod/lmod/share/man:.:;
+export PATH=/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-v100/bin:/root/spack/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/cuda/bin:/opt/rocm/bin;
+export PKG_CONFIG_PATH=/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-v100/share/pkgconfig:/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-v100/lib64/pkgconfig:/srv/storage/kerdatalille@storage1.lille.grid5000.fr/view/chifflot-v100/lib/pkgconfig;
