179ff285b4eab3feecfa459bb37f30e5b26f23d2
Malvin Chevallier <malvin.chevallier@gmail.com>
2023-08-11 14:06:56 +0200

    feat: add sweep automation

---
+++ e2clab/hparams/chifflot/2/sweep_launcher.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+HOST="$1"
+
+cd /root/distributed-continual-learning/ && sed -i "s/host=.*/host='$HOST'/g" "sweep.py"
+
+cd /root/distributed-continual-learning/ && git config --global --add safe.directory /root/distributed-continual-learning
+
+cd /root/distributed-continual-learning/ && wandb sweep sweep.yaml --project distributed-continual-learning 2>&1 | eval $(grep -o "wandb agent .*")
