a5a90bd943ea90ccc55a76ff81f52ea17d36437c
Thomas Bouvier <contact@thomas-bouvier.io>
2023-11-18 18:00:41 +0100

    feat: add support for ptychography sweeps

--- e2clab/hparams/chifflot/2-v100-ptycho/sweep_launcher.sh
+++ e2clab/hparams/chifflot/2-v100-ptycho/sweep_launcher.sh
@@ -6,4 +6,4 @@ cd /root/distributed-continual-learning/ && sed -i "s/host = .*/host = \"$HOST\"
 
 cd /root/distributed-continual-learning/ && git config --global --add safe.directory /root/distributed-continual-learning
 
-cd /root/distributed-continual-learning/ && wandb sweep sweep.yaml --project distributed-continual-learning 2>&1 | eval $(grep -o "wandb agent .*")
+cd /root/distributed-continual-learning/ && wandb sweep sweep.yaml --project continual-ptycho 2>&1 | eval $(grep -o "wandb agent .*")
