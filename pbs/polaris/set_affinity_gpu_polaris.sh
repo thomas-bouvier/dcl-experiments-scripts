1d208426298443c9e8843c45e1fbcaba92a6550b
Thomas Bouvier <contact@thomas-bouvier.io>
2024-05-27 11:28:59 +0200

    feat(polaris): add job submission scripts

---
+++ pbs/polaris/set_affinity_gpu_polaris.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh -l
+
+# Check if PMI_LOCAL_RANK is set
+if [[ -z "${PMI_LOCAL_RANK}" ]]; then
+    echo "PMI_LOCAL_RANK is not set. Exiting..."
+    exit 1
+fi
+
+num_gpus=$(nvidia-smi -L | wc -l)
+gpu=$((${PMI_LOCAL_RANK} % ${num_gpus}))
+
+gpus=""
+for i in $(seq 0 $(($num_gpus-1))); do
+  gpus="$gpus$i,"
+done
+
+export CUDA_VISIBLE_DEVICES=${gpus%?}
+echo “RANK=${PMI_RANK} LOCAL_RANK=${PMI_LOCAL_RANK}”
+
+exec "$@"
